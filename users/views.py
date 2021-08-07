609050d3ce0cb9e714f006eb94966c9f4a870a31
Summer <summmick@gmail.com>
2021-08-07 12:01:28 -0700

    Render all the products for a profile

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
-
+from users.models import Profile
 
 def register(request):
     if request.method == 'POST':
@@ -19,4 +19,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    user = request.user;
+    profile = Profile.objects.get(user=user)
+    all_products = profile.products.all()
+
+    return render(request, 'users/profile.html', {'products': all_products})
