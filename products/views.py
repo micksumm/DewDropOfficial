0a323c6f4d76bfc5d06440b3ec0ba918338c2880
Summer <summmick@gmail.com>
2021-08-07 12:09:55 -0700

    Add settings.py to version control

--- products/views.py
+++ products/views.py
@@ -29,7 +29,7 @@ class ProductGetByCondition(generics.ListAPIView):
 
 
 @login_required()
-def add_to_profile(request, **kwargs):
+def add_to_profile(request):
     # Get Profile given the logged in User
     user = request.user;
     profile = Profile.objects.get(user=user)
@@ -42,26 +42,5 @@ def add_to_profile(request, **kwargs):
     profile.products.add(product)
     profile.save()
 
-
-
-    # get the user profile
-    # user_profile = get_object_or_404(Profile, user=request.user)
-    # # filter products by id
-    # product = Product.objects.filter(id=kwargs.get('item_id', "")).first()
-    # # check if the user already owns this product
-    # if product in request.user.profile.ebooks.all():
-    #     messages.info(request, 'You already own this ebook')
-    #     return redirect(reverse('products:product-list')) 
-    # # create orderItem of the selected product
-    # order_item, status = OrderItem.objects.get_or_create(product=product)
-    # # create order associated with the user
-    # user_order, status = Order.objects.get_or_create(owner=user_profile, is_ordered=False)
-    # user_order.items.add(order_item)
-    # if status:
-    #     # generate a reference code
-    #     user_order.ref_code = generate_order_id()
-    #     user_order.save()
-
-    # # show confirmation message and redirect back to the same page
-    # messages.info(request, "item added to cart")
+    # TODO: Do not render about.html
     return render(request, 'products/about.html', {})
